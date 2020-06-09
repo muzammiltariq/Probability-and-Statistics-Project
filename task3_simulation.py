92f058443b299bc9eddafa97f1724e253185a4ef
Muzammil Tariq <46838708+muzammiltariq@users.noreply.github.com>
2020-06-09 22:54:01 +0500

    Improved task 3

--- task3_simulation.py
+++ task3_simulation.py
@@ -7,10 +7,15 @@ bob.circle(100)
 node.penup()
 node.goto(0,100)
 node.pendown()
+node.color("green")
 while True:
+    move = random.choice(moves)*10
+    deg = random.randint(0,360)
     current = ((node.pos()[0]-0)**2) + ((node.pos()[1]-100)**2)
-    print(current,100**2)
-    if current < (100**2):
-        node.right(180)
-    node.forward(random.choice(moves)*100)
-    node.right(random.randint(0,360))
+    print(current)
+    if current >= (100**2):
+        node.goto(0,100) # Takes the node back to the centre of the circle
+    #if (node.pos()[0] >= 100 or node.pos()[0] <= -100) or (node.pos()[1] >= 100 or node.pos()[1] <= -100):
+    #    node.goto(0,100)
+    node.forward(move)
+    node.right(deg)
