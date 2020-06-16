40263bba8cd4aa2f03cfa6ceb6ca7f823b7492ae
Farazozair <49784736+Farazozair@users.noreply.github.com>
2020-06-17 01:24:11 +0500

    Update task4_simulation.py

--- task4_simulation.py
+++ task4_simulation.py
@@ -8,12 +8,18 @@ for i in range(10):
     numbers.append(random.random())
 print(numbers)
 bob = turtle.Turtle()
-bob.speed(3)
-
-while True:
+bob.speed(1)
+pos1 = bob.pos()
+for i in range(100):  # this range could be change to observe more number of trials, e.g. for i in range(1000)
     choose = random.choice(numbers)
     if (choose > 0.5):
+        bob.color("red")
         bob.forward(random.uniform(0, 1))
-    elif (choose < 0.5):
+    if (choose < 0.5):
+        bob.color("green")
         bob.backward(random.uniform(0, 1))
+pos2 = bob.pos()
+print(pos2)
+distance = pos1[0]-pos2[0]
+print("Distance from start position is:", round(abs(distance), 3))
 turtle.mainloop()
