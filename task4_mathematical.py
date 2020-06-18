ea83a15107403b62a05f617d219572bfdb4a9176
Farazozair <49784736+Farazozair@users.noreply.github.com>
2020-06-18 15:09:22 +0500

    Update task4_mathematical.py

--- task4_mathematical.py
+++ task4_mathematical.py
@@ -1,11 +1,35 @@
+# import matplotlib.pyplot as plt
+# import math
+# from scipy.stats import uniform
+
+# n = 1000
+# start = 0
+# width = 1
+
+# data_uniform = uniform.rvs(size=n, loc=start, scale=width)
+# plt.plot(data_uniform)
+# plt.show()
+
+import random
 import matplotlib.pyplot as plt
 import math
-from scipy.stats import uniform
+random.seed()
+prob = [0.5, 0.5, 0]  # Change this to change the probability
 
-n = 1000
-start = 0
-width = 1
+start = 0   # Change this to change starting position
+positions = [start]
 
-data_uniform = uniform.rvs(size=n, loc=start, scale=width)
-plt.plot(data_uniform)
+numbers = []
+for i in range(50):
+    numbers.append(random.randint(0, 1000)/1000.0)
+for i in numbers:
+    if i == prob[2]:
+        positions.append(positions[-1])
+    elif i < prob[0]:
+        positions.append(positions[-1]-1)
+    elif i > prob[1]:
+        positions.append(positions[-1]+1)
+
+plt.plot(positions)
 plt.show()
+print(math.inf)
